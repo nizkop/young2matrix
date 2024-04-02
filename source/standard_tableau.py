b5e439791399c1f2d5fb3c4561613378d6b084aa
Judith Leson <judith.leson@stud.uni-duisburg-essen.de>
2024-04-02 19:08:09 +0200

    first tableau prints

--- source/standard_tableau.py
+++ source/standard_tableau.py
@@ -1,37 +1,54 @@
 from typing import List
 
-from source.function_combination import function_combination
-from source.function_parts.spatial_part import spatial_part
-from source.function_parts.spin_part import spin_part
 from source.young_tableau import young_tableau
 
 
 class standard_tableau(young_tableau):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, numbers_in_row:List[List[int]] = []):
+        super().__init__(number_of_rows=len(numbers_in_row), number_of_columns=[len(i) for i in numbers_in_row])
         self.permutation_group:int=0
-        self.help: function_combination = function_combination()
-        self.spatial_parts: List[spatial_part] = []
-        self.spin_parts: List[spin_part] = []
+        self.numbers_in_row:List[List[int]] = numbers_in_row
+        self.check()
 
     def print(self):
-        pass
+        for i in range(self.number_of_rows):
+            for j in range(self.number_of_columns[i]):
+                print(f"[{self.numbers_in_row[i][j]}]", end=" ")
+            print()
+
+    def check(self):
+        super().check()
+        if not hasattr(self, 'numbers_in_row'):
+            return # not initialized yet
+        all_numbers = [element for sublist in self.numbers_in_row for element in sublist]
+        if len(set(all_numbers)) != len(all_numbers):
+            raise Exception("standard_tableau: no duplicate numbers allowed")
+        for row in self.numbers_in_row:
+            if not all(row[i] <= row[i + 1] for i in range(len(row) - 1)):
+                raise Exception("standard_tableau: numbers have to be ascending in a row")
+        for column in self.get_numbers_in_columns():
+            if not all(column[i] <= column[i+1] for i in range(len(column)-1)):
+                raise Exception("standard_tableau: numbers have to be ascending in a column")
+
+    # def get_numbers_in_columns(self) -> List[List[int]]:
+    #     """ reverting the list of numbers in a row into a list of numbers in a column """
+    #     max_length = max(len(row) for row in self.numbers_in_row)
+    #     spalten = []
+    #     for i in range(max_length):
+    #         column = []
+    #         for row in self.numbers_in_row:
+    #              if i < len(row):
+    #                  column.append(row[i])
+    #         spalten.append(column)
+    #     return spalten
+    #     #  return [[row[i] for row in self.numbers_in_row if i < len(row)] for i in range(max_length)]
+
 
     def to_text(self):
         pass
 
-    def solve(self):
-        pass
 
-    def get_spatial_choices(self):
-        pass
-
-    def calulate_all_overlap_integrals(self):
-        pass
-
-    def calculate_all_hamilton_integrals(self):
-        pass
-
-    def get_spin_choices(self):
-        pass
+if __name__ == '__main__':
+    s = standard_tableau([[1,2,4],[3,5]])
+    s.print()
--- source/young_tableau.py
+++ source/young_tableau.py
@@ -2,16 +2,41 @@ from typing import List
 
 
 class young_tableau(object):
-    def __init__(self):
+    def __init__(self, number_of_rows:int=0, number_of_columns:List[int]=[]):
         self.number_of_parts:int=0
-        self.number_of_rows:int=0
-        self.number_of_columns:List[int]=[]
+        self.number_of_rows:int=number_of_rows
+        self.number_of_columns:List[int]=number_of_columns
+        self.check()
+
+    def check(self):
+        if len(self.number_of_columns) != self.number_of_rows:
+            raise Exception("young_tableau: non-fitting dimensions")
+        if not all(self.number_of_columns[i] >= self.number_of_columns[i + 1] for i in range(len(self.number_of_columns) - 1)):
+            raise Exception("young_tableau: unfulfilled young rule")
+
 
     def print(self):
-        pass
+        for i in range(self.number_of_rows):
+            for j in range(self.number_of_columns[i]):
+                print("[ ]", end=" ")
+            print()
 
     def to_text(self):
         pass
 
     def get_all_options(self):
         pass
+
+    def get_permutation_group(self) -> int:
+        """ calculate the number of boxes in the tableau """
+        sum = 0
+        for i in range(self.number_of_rows):
+            sum += self.number_of_columns[i]
+        return sum
+
+
+
+if __name__ == '__main__':
+    y = young_tableau(2, [2,1])
+    y.print()
+    print(y.get_permutation_group())
