8bddf5878e6ad27a102328d685339c4d4048c625
Judith Leson <judith.leson@stud.uni-duisburg-essen.de>
2024-04-03 09:37:30 +0200

    permutation group with non-adjoint tableaus and print

--- source/permutation_group.py
+++ source/permutation_group.py
@@ -16,7 +16,10 @@ class permutation_group(object):
         self.overview = overview_pdf()
 
     def print(self):
-        pass
+       print(f"permutation group: S_{self.permutation_group}")
+       for s in p.get_non_adjoint_tableaus():
+           s.print()
+           print()
 
     def find_tableaus(self):
         pass
@@ -32,22 +35,20 @@ class permutation_group(object):
         numbers = list(range(1, self.permutation_group + 1))
         subsets = [list(subset) for subset in find_subsets(numbers) if len(subset) > 0]
 
+        # combine subsets into tableaus:
         for possible_tableau in list(powerset(subsets, self.permutation_group)):
             if len(possible_tableau) > 0:
                 test_tableau = standard_tableau(possible_tableau)
                 if test_tableau.permutation_group == self.permutation_group and test_tableau.check():
                         self.standard_tableaus.append(test_tableau)
 
-        # # combine subsets into tableaus:
-        # for i in range(1,self.permutation_group+1):
-        #     for possible_tableau in list(itertools.combinations(subsets,i)):
-        #         test_tableau = standard_tableau(possible_tableau)
-        #         if test_tableau.permutation_group == self.permutation_group and test_tableau.check():
-        #             self.standard_tableaus.append(test_tableau)
-        #         else:
-        #              print("wrong", possible_tableau)
-
-
+    def get_non_adjoint_tableaus(self):
+        """ choosing one orientation of tableaus (here: vertical alignment favored)
+        e.g.: [1,2] adjoint to [1][2]
+        """
+        if len(self.standard_tableaus) == 0:
+            self.get_all_standard_tableaus()
+        return [t for t in self.standard_tableaus if max(t.number_of_columns) <= t.number_of_rows]
 
     def calculate_all_overlap_integrals(self):
         pass
@@ -63,6 +64,4 @@ if __name__ == '__main__':
     p = permutation_group(4)
     p.get_all_standard_tableaus()
 
-    for s in p.standard_tableaus:
-        s.print()
-        print()
+    p.print()
