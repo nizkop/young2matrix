24d23fe095b17457a53abdd7a7afb752ba91a42f
Judith Leson <judith.leson@stud.uni-duisburg-essen.de>
2024-04-20 08:36:19 +0200

    test_standard_tableau again with all tests

--- source/tests/test_standard_tableau.py
+++ source/tests/test_standard_tableau.py
@@ -3,22 +3,38 @@ from source.standard_tableau import standard_tableau
 
 # ! these tests are sensitive to the order of summands (= an error might not be a mistake)
 trials = [
-    # # single operators:
-    # {"input": [(1, 2)], "expected": ["+a1b2", "+a2b1"], "error_message": "symmetrizing wrong"},
-    # {"input": [(1,),(2,)], "expected": ["+a1b2", "-a2b1"], "error_message": "anti-symmetrizing wrong"},
-    # {"input": [(1,2,3)], "expected": ["+a1b2c3","+a1b3c2","+a2b1c3", "+a2b3c1", "+a3b1c2", "+a3b2c1"], "error_message": "symmetrizing wrong (> 2)"},
-    # {"input": [(1,),(2,),(3,)], "expected": ["+a1b2c3","-a1b3c2", "-a2b1c3", "+a2b3c1", "+a3b1c2", "-a3b2c1"], "error_message": "anti-symmetrizing wrong (> 2)"},
-    # # mixed terms:
-    # {"input": [(1,2),(3,)], "expected": ["+a1b2c3", "-a3b2c1", "+a2b1c3" , "-a2b3c1"], "error_message": "anti- & symmetrizing wrong"},
-    # {"input": [(1,3),(2,)], "expected": ["+a1b2c3", "-a2b1c3", "-a3b1c2" , "+a3b2c1"], "error_message": "anti- & symmetrizing wrong"},
+    ## examples from weißbluth:
+    # single operators:
+    {"input": [(1, 2)], "expected": {"a1b2": Sign.PLUS, "a2b1": Sign.PLUS}, "error_message": "symmetrizing wrong"},
+    {"input": [(1,),(2,)], "expected": {"a1b2": Sign.PLUS, "a2b1": Sign.MINUS}, "error_message": "anti-symmetrizing wrong"},
+    {"input": [(1,2,3)], "error_message": "symmetrizing wrong (> 2)",
+     "expected": {"a1b2c3": Sign.PLUS,"a1b3c2": Sign.PLUS,"a2b1c3": Sign.PLUS, "a2b3c1": Sign.PLUS, "a3b1c2": Sign.PLUS, "a3b2c1": Sign.PLUS} },
+    {"input": [(1,),(2,),(3,)], "error_message": "anti-symmetrizing wrong (> 2)",
+     "expected": {"a1b2c3": Sign.PLUS,"a1b3c2": Sign.MINUS, "a2b1c3": Sign.MINUS, "a2b3c1": Sign.PLUS, "a3b1c2": Sign.PLUS, "a3b2c1": Sign.MINUS}},
+    # mixed terms:
+    {"input": [(1,2),(3,)], "error_message": "anti- & symmetrizing wrong",
+     "expected": {"a1b2c3": Sign.PLUS, "a3b2c1": Sign.MINUS, "a2b1c3": Sign.PLUS , "a2b3c1": Sign.MINUS}},
+    {"input": [(1,3),(2,)], "error_message": "anti- & symmetrizing wrong",
+     "expected": {"a1b2c3": Sign.PLUS, "a2b1c3": Sign.MINUS, "a3b1c2": Sign.MINUS , "a3b2c1": Sign.PLUS}},
 
-    {"input": [(1,2),(3,4)],
+    ## manually calculated: (1. sym, 2. anti)
+    {"input": [(1,2),(3,4)],  "error_message": "S4 [2^2] 12-34",
      "expected": {"a1b2c3d4":Sign.PLUS, "a2b1c3d4": Sign.PLUS, "a1b2c4d3": Sign.PLUS, "a2b1c4d3": Sign.PLUS ,
                   "a3b2c1d4": Sign.MINUS , "a2b3c1d4": Sign.MINUS , "a3b2c4d1": Sign.MINUS , "a2b3c4d1": Sign.MINUS ,
                   "a1b4c3d2": Sign.MINUS , "a4b1c3d2": Sign.MINUS , "a1b4c2d3": Sign.MINUS, "a4b1c2d3": Sign.MINUS,
-                  "a3b4c1d2": Sign.PLUS, "a4b3c1d2": Sign.PLUS, "a3b4c2d1": Sign.PLUS, "a4b3c2d1": Sign.PLUS},
-     "error_message": "S4"
-     }
+                  "a3b4c1d2": Sign.PLUS, "a4b3c1d2": Sign.PLUS, "a3b4c2d1": Sign.PLUS, "a4b3c2d1": Sign.PLUS}
+     },
+    {"input": [(1, 3), (2, 4)], "error_message": "S4 [2^2] 13-24",
+     "expected": {"a1b2c3d4": Sign.PLUS, "a3b2c1d4": Sign.PLUS, "a1b4c3d2": Sign.PLUS, "a3b4c1d2": Sign.PLUS,
+                  "a2b1c3d4": Sign.MINUS, "a3b1c2d4": Sign.MINUS, "a2b4c3d1": Sign.MINUS, "a3b4c2d1": Sign.MINUS,
+                  "a1b2c4d3": Sign.MINUS, "a4b2c1d3": Sign.MINUS, "a1b3c4d2": Sign.MINUS, "a4b3c1d2": Sign.MINUS,
+                  "a2b1c4d3": Sign.PLUS, "a4b1c2d3": Sign.PLUS, "a2b3c4d1": Sign.PLUS, "a4b3c2d1": Sign.PLUS}
+     },
+    {"input": [(1,3),(2,),(4,)], "error_message": "S4 [21^1] 13-2-4",
+     "expected": {
+            "a1b2c3d4": Sign.PLUS , "a2b1c3d4": Sign.MINUS , "a1b4c3d2": Sign.MINUS , "a4b2c3d1": Sign.MINUS, "a2b4c3d1": Sign.PLUS , "a4b1c3d2": Sign.PLUS ,
+            "a3b2c1d4": Sign.PLUS , "a3b1c2d4": Sign.MINUS , "a3b4c1d2": Sign.MINUS , "a3b2c4d1": Sign.MINUS , "a3b1c4d2": Sign.PLUS , "a3b4c2d1": Sign.PLUS }
+     },
     # {"input": [], "expected": "+ ", "error_message": ""},
 ]
 
@@ -31,9 +47,7 @@ for i in trials:
     if not s.function.get_number_of_terms() == len(i["expected"].keys()):
         raise Exception("wrong number of terms: "+i["error_message"])
     for p in s.function.parts:
-        # print()
         actual = p.to_text().replace("* ","").replace("_","").replace(" ","").replace('-',"")
-        # print(actual)
         if "+" in actual:
             sign = Sign.PLUS
             actual = actual.replace("+","")
@@ -46,23 +60,6 @@ for i in trials:
                 raise Exception(i["error_message"] + f": wrong sign for {actual}, {i['error_message']}")
         except KeyError:
             Exception(i["error_message"] + f": {[actual]} not included in {expected.keys()}")
-        # try:
-        #     index = [y.replace("−", "").replace("-","").replace("+","") for y in expected].index(actual)
-        # except ValueError:
-        #     print( [y.replace("−", "").replace("-","").replace("+","") for y in expected] )
-        #     raise Exception(i["error_message"] + f": {[actual]} vs. {[y.replace('−','') for y in expected]}")
-        #
-        # if sign == "+":
-        #     if sign not in expected[index]:
-        #         raise Exception(i["error_message"] + f": wrong sign for {actual}, {i['error_message']}")
-        # else:
-        #     if "+" in expected[index]:
-        #         raise Exception(i["error_message"] + f": wrong sign for {actual}, {i['error_message']}")
 
-
-
-    # actual = s.function.to_text().replace(" ","").replace("*","")
-    # if not actual == expected.replace(" ","").replace("*","") :
-    #     raise Exception(i["error_message"]+": "+ actual+ " vs. "+ expected)
 
 
