eb98a349c4960fd76c3e133e45610770748f5247
Judith Leson <judith.leson@stud.uni-duisburg-essen.de>
2024-05-05 18:56:54 +0200

    fixed: button explanation didnt change language

--- source/ui_parts/MainApplication.py
+++ source/ui_parts/MainApplication.py
@@ -1,21 +1,10 @@
 
 from matplotlib import pyplot as plt
-
-
-
-# plt.rcParams['text.usetex'] = True
-# plt.rcParams['text.latex.preamble'] = r'''
-# \usepackage{mathtools}
-# \usepackage{tocloft}
-# \usepackage{physics}
-# \usepackage{breqn}
-# '''
-
 from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QPushButton, QLabel, \
     QScrollArea, QStatusBar
 
 from source.ui_parts.get_latex_canvas import get_latex_canvas
-from source.ui_parts.ui_pages import ui_pages
+from source.ui_parts.ui_pages import ui_pages, get_page_name
 from source.ui_parts.settings.idea_config import update_language, get_language
 
 
@@ -47,33 +36,34 @@ class MainApplication(QMainWindow):
         self.current_page = 0
         self.pages = [
             # TODO english names for pages ?
-            {"name": "Startseite", "sign": "start" if get_language()=="en" else "Start",
+            {"sign": "start" if get_language()=="en" else "Start",
                     "index": ui_pages.START, "function": self.load_main_page, "parent": None,
                     "buttons": [ui_pages.TABLEAUS, ui_pages.SPIN, ui_pages.SPATIAL_FUNCTIONS, ui_pages.DOWNLOAD]},
-            {"name": "Tableaus", "sign": "[1][2]", "index": ui_pages.TABLEAUS, "function": self.load_tableau_page, "parent": ui_pages.START,
+            {"sign": "[1][2]", "index": ui_pages.TABLEAUS, "function": self.load_tableau_page, "parent": ui_pages.START,
                     "buttons": [ui_pages.START, ui_pages.MULTIPLIED_OUT_TABLEAUS, ui_pages.SPIN, ui_pages.SPATIAL_FUNCTIONS]},
-            {"name": "Ausmultiplizierte Tableaus", "sign": "ausmultiplizieren", "index": ui_pages.MULTIPLIED_OUT_TABLEAUS,
+            {"sign": "ausmultiplizieren" if get_language() == "de" else "multiply out", "index": ui_pages.MULTIPLIED_OUT_TABLEAUS,
                     "buttons": [ui_pages.START, ui_pages.TABLEAUS, ui_pages.SPIN, ui_pages.SPATIAL_FUNCTIONS],
                     "function": self.load_tableau_page_multiplied, "parent": ui_pages.TABLEAUS},
-            {"name": "Spinfunktionen","sign": "σ", "index": ui_pages.SPIN, "function": self.load_spin_page, "parent": ui_pages.START,
+            {"sign": "σ", "index": ui_pages.SPIN, "function": self.load_spin_page, "parent": ui_pages.START,
                     "buttons": [ui_pages.START, ui_pages.TABLEAUS, ui_pages.SPATIAL_FUNCTIONS, ui_pages.OVERLAP_SPIN, ui_pages.HAMILTON_SPIN]},
-            {"name": "Raumfunktionen", "sign": "Φ", "index": ui_pages.SPATIAL_FUNCTIONS,
+            {"sign": "Φ", "index": ui_pages.SPATIAL_FUNCTIONS,
                     "function": self.load_spatial_page, "parent": ui_pages.START,
                     "buttons": [ui_pages.START, ui_pages.TABLEAUS, ui_pages.SPIN, ui_pages.OVERLAP_SPATIAL, ui_pages.HAMILTON_SPATIAL]},
-            {"name": "Download", "sign": "⤓"  # "⬇️" ↓ ⬇  ⤓
+            {"sign": "⤓"  # "⬇️" ↓ ⬇  ⤓
                     , "index": ui_pages.DOWNLOAD, "function": self.load_download, "parent": ui_pages.START, "buttons": [ui_pages.START]},
-            {"name": "Überlapp Spin", "sign": "<|> (σ)", "index": ui_pages.OVERLAP_SPIN,
+            {"sign": "<|> (σ)", "index": ui_pages.OVERLAP_SPIN,
                     "function": self.load_overlap_spin, "parent": ui_pages.SPIN,
                     "buttons": [ui_pages.START, ui_pages.SPIN, ui_pages.OVERLAP_SPATIAL, ui_pages.HAMILTON_SPIN]},
-            {"name": "Überlapp Raum", "sign": "<|> (Φ)", "index": ui_pages.OVERLAP_SPATIAL,
+            {"sign": "<|> (Φ)", "index": ui_pages.OVERLAP_SPATIAL,
                     "function": self.load_overlap_spatial, "parent": ui_pages.SPATIAL_FUNCTIONS,
                     "buttons": [ui_pages.START, ui_pages.SPIN, ui_pages.OVERLAP_SPIN, ui_pages.HAMILTON_SPATIAL]},
-            {"name": "Hamilton Spin", "sign": "H (σ)" , "index": ui_pages.HAMILTON_SPIN, "function": self.load_hamilton_spin,
+            {"sign": "H (σ)" , "index": ui_pages.HAMILTON_SPIN, "function": self.load_hamilton_spin,
                     "parent": ui_pages.OVERLAP_SPIN, "buttons": [ui_pages.START, ui_pages.OVERLAP_SPIN, ui_pages.SPIN]},
-            {"name": "Hamilton Raum", "sign": "H (Φ)" , "index": ui_pages.HAMILTON_SPATIAL, "function": self.load_hamilton_spatial,
+            {"sign": "H (Φ)" , "index": ui_pages.HAMILTON_SPATIAL, "function": self.load_hamilton_spatial,
                     "parent": ui_pages.OVERLAP_SPATIAL, "buttons": [ui_pages.START, ui_pages.OVERLAP_SPIN, ui_pages.SPATIAL_FUNCTIONS]}
-
         ]
+        for p in range(len(self.pages)):
+            self.pages[p]["name"] = get_page_name(self.pages[p]["index"])
 
         self.label = QLabel()
         self.label.setStyleSheet("background-color: transparent;")
@@ -84,14 +74,14 @@ class MainApplication(QMainWindow):
         self.create_widgets()
 
 
-    def create_language_buttons(self):
-        """ """
+    def create_language_buttons(self) -> None:
+        """ adding a button, that may change the language settings, to the top of the screen """
         if get_language() == "en":
             choice = "de"
             info ="change language to German"
         else:
             choice = "en"
-            info = "Sprache zu English ändern"
+            info = "Sprache zu Englisch ändern"
 
         self.language_button = QPushButton(choice)
         self.language_button.setStyleSheet(f"background-color: {self.color};")
@@ -104,14 +94,23 @@ class MainApplication(QMainWindow):
         self.language_button.enterEvent = lambda event, button= self.language_button: self.statusBar.showMessage( self.language_button.toolTip())
         self.language_button.leaveEvent = lambda event: self.statusBar.clearMessage()
 
-    def set_language(self, language: str):
+    def set_language(self, language: str) -> None:
+        """ updating the language (as it was changed by the user)
+        :param language: abbreviation for the new language ('en' or 'de')
+        """
         update_language(language)
+        for p in range(len(self.pages)):# update pages names
+            self.pages[p]["name"] = get_page_name(self.pages[p]["index"])
         self.language_button.deleteLater()
         self.create_language_buttons()
         self.open_page(self.current_page)
-        # self.change_page(self.current_page)
 
-    def create_widgets(self):
+
+    def create_widgets(self) -> None:
+        """
+        setting up the buttons for the current page and linking them to following pages / information;
+        because the buttons shall be at the bottom of the screen, first the creation of all other content of the page is initialized
+        """
         # print("create widgets", flush=True)
         self.update_page() #content above buttons
 
@@ -121,8 +120,10 @@ class MainApplication(QMainWindow):
         button_layout = QHBoxLayout()  # horizontal layout (for buttons)
         for page_info in self.pages:
             if page_info["index"].value != self.current_page and page_info["index"] in current_page_info["buttons"]:
-                sign = f"zurück zu: {page_info['sign']}" \
-                    if page_info["index"] == current_page_info["parent"] else page_info["sign"]
+                if page_info["index"] == current_page_info["parent"]:
+                    sign = f"zurück zu: {page_info['sign']}" if get_language() == "de" else f"back to: {page_info['sign']}"
+                else:
+                    sign = page_info["sign"]
 
                 button = QPushButton(sign)
                 button.setStyleSheet(f"background-color: {self.color};")
@@ -137,23 +138,24 @@ class MainApplication(QMainWindow):
         self.scroll_layout.addLayout(button_layout)
         self.non_basics.append(button_layout)
 
-    def open_page(self, page_number:int):
+    def open_page(self, page_number:int) -> None:
         """
         to be implemented in sub-class
-        :param i:
-        :return:
+        :param page_number: number of the page to be opened
+        :return: None
         """
         return self.change_page(page_number)
 
-    def update_page(self):
+    def update_page(self) -> None:
         # print("update_page", flush=True)
         formulas = [r"E = m \cdot c^ 2", r"\frac{1}{2}", "\\begin{array}{c}{1}\\end{array}", r"\bra{1}"]# test equations
 
         for formula in formulas:
             self.add_equation(formula)
 
-    def add_equation(self, formula: str):
+    def add_equation(self, formula: str) -> None:
         """
+        adding a line with an equation to the current screen/page
         :param formula: latex-formatted equation, e.g. r"\frac{1}{2} \cdot \pi"
         """
         # print("add_equation", flush=True)
@@ -164,38 +166,20 @@ class MainApplication(QMainWindow):
         plt.close()
 
 
-    def change_page(self, index: int):
-        # print("change page",flush=True)
+    def change_page(self, index: int) -> None:
+        """
+        changing a page by removing the old content and adding the new
+        :param index: new page number
+        """
         self.clearLayout()
         self.current_page = index
         self.create_widgets()
-        # self.update_page()
 
 
     def clearLayout(self) -> None:
         """ Clearing the current layout and its sublayouts """
-        print("clear layout",flush=True)
+        # print("clear layout",flush=True)
         for i in range(len(self.non_basics)-1,-1,-1):
             item = self.non_basics[i]
             item.deleteLater()
             del self.non_basics[i]
-        # if layout is None:
-        #     layout = self.layout
-        # while layout.count():
-        #     item = layout.takeAt(0)
-        #     widget = item.widget()
-        #     if widget is not None:
-        #         print(widget)
-        #         widget.deleteLater()
-        #     else:
-        #         sublayout = item.layout()
-        #         if sublayout:
-        #             self.clearLayout(layout=sublayout)
-
-
-
-# if __name__ == "__main__":
-#     app = QApplication(sys.argv)
-#     window = MainApplication()
-#     window.show()
-#     sys.exit(app.exec_())
--- source/ui_parts/ui_pages.py
+++ source/ui_parts/ui_pages.py
@@ -1,5 +1,7 @@
 from enum import Enum
 
+from source.ui_parts.settings.idea_config import get_language
+
 
 class ui_pages(Enum):
     START = 0
@@ -13,5 +15,35 @@ class ui_pages(Enum):
     HAMILTON_SPIN = 8
     HAMILTON_SPATIAL = 9
 
+
+
+def get_page_name(page:ui_pages) -> str:
+    """
+    get the explanation of a button leading to a given page;
+    regarding the current language
+    :param page: screen the button leads to
+    :return:
+    """
+    if page == ui_pages.START:
+        return "Startseite" if get_language() == "de" else "start"
+    if page == ui_pages.TABLEAUS:
+        return "Tableaus" if get_language() == "de" else "tableaus"
+    if page == ui_pages.MULTIPLIED_OUT_TABLEAUS:
+        return "Ausmultiplizierte Tableaus" if get_language() == "de" else "multiplied out tableaus"
+    if page == ui_pages.SPIN:
+        return "Spinfunktionen" if get_language() == "de" else "spin functions"
+    if page == ui_pages.SPATIAL_FUNCTIONS:
+        return "Raumfunktionen" if get_language() == "de" else "spatial functions"
+    if page == ui_pages.DOWNLOAD:
+        return "Download" if get_language() == "de" else "download"
+    if page == ui_pages.OVERLAP_SPIN:
+        return "Überlapp Spin" if get_language() == "de" else "overlap of spin functions"
+    if page == ui_pages.OVERLAP_SPATIAL:
+        return "Überlapp Raum" if get_language() == "de" else "overlap of spatial functions"
+    if page == ui_pages.HAMILTON_SPIN:
+        return "Hamilton Spin" if get_language() == "de" else "hamilton integrals for spin"
+    if page == ui_pages.HAMILTON_SPATIAL:
+        return "Hamilton Raum" if get_language() == "de" else "hamilton integrals for spatial functions"
+    return "unbekannt" if get_language() == "de" else "unknown"
 
 
